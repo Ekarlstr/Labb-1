279c9b20a0e4e02c69ed36a8dc047a3ec2d231b0
Erik <ekarlstr@kth.se>
2018-09-03 11:18:45 +0200

    Third commit

---
+++ Labb1_4.py
@@ -0,0 +1,23 @@
+def kubsumma(n):
+    a=0
+    b=0
+    count=0
+    list = []
+    while True:
+        if n== a**3 + b**3 and a>=b:
+            list.append((a,b))
+            count=count+1
+        a=a+1
+        if a**3 > n:
+            a=b
+            b=b+1
+        if count==0 and b**3 > n:
+            list.append("Kubsumma saknas!")
+            break
+        if b**3 > n:
+            break
+    return list
+while True:
+    n=int(input("Säg ett nummer för att hitta möjliga kubsummor: "))
+    kubsumma(n)
+    print("Kubsumma:",list)
